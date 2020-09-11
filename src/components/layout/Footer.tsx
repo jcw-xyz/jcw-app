14e647735b70c34aa5002b7661522684950e4a40
jameshih <j@shih.app>
2020-09-11 00:53:15 -0400

    deleted footer

--- src/components/layout/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Container } from '../core';
-
-export default function Footer() {
-    return <Container className={'fixed bottom-0'}>footer</Container>;
-}
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,6 @@
 import Header from './Header';
-import Footer from './Footer';
 import Layout from './Layout';
 import Nav from './Nav';
 import ConnectWS from './ConnectWS';
 
-export { Header, Footer, Layout, Nav, ConnectWS };
+export { Header, Layout, Nav, ConnectWS };
