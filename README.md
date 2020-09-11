5eb46ac58b7d587b6d2255d8d4d57766a3edf31f
jameshih <j@shih.app>
2020-09-11 16:06:58 -0400

    update readme

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 -   [x] live profit page
 -   [x] add live profit card bar animation
 -   [x] recent blocks page
+-   [ ] fix block card img issue
 -   [ ] charts page
 -   [ ] search page
--- src/components/pages/RecentBlocks/BlockCard.tsx
+++ src/components/pages/RecentBlocks/BlockCard.tsx
@@ -32,8 +32,15 @@ export default function BlockCard({
     is_stale: boolean;
 }) {
     const img_url: any = useSelector<RootState>((state) => state.api.img_url);
-    const coin = _.find(img_url, (o) => (o.name === coin_name ? o.image_filename : 'none'));
-    const imgPath = `https://images.prohashing.com/coins/${coin && coin.image_filename}`;
+    const imgPath = _.find(img_url, (o) => {
+        if (o.name.trim() === coin_name.trim()) {
+            console.log(o);
+            return `https://images.prohashing.com/coins/${o.image_filename}`;
+        } else {
+            console.log(img_url);
+            return `https://images.prohashing.com/coins/${o.image_filename}`;
+        }
+    });
 
     const [now] = useState(new Date());
 
