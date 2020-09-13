eb801a1b79dedd4c3714d6951d6d870a22fa7b26
jameshih <j@shih.app>
2020-09-13 00:57:36 -0400

    Updates

---
+++ precache-manifest.dc4937a0bd310e932dac63c2a12d9a14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520fad15a93ce48b9dae2c0b5926792f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e27ea73c22911095c03d",
+    "url": "/static/css/main.5b9da114.chunk.css"
+  },
+  {
+    "revision": "9c5a24cc1ac517ccb071",
+    "url": "/static/js/2.0d6d1735.chunk.js"
+  },
+  {
+    "revision": "4017a429e6d2b81ead9a503062f2ec4e",
+    "url": "/static/js/2.0d6d1735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b60f7033847d8b03ce",
+    "url": "/static/js/3.fab1f4f6.chunk.js"
+  },
+  {
+    "revision": "c7643e7d36ae44b03eb8",
+    "url": "/static/js/4.696b00a8.chunk.js"
+  },
+  {
+    "revision": "1f1ae03d5350cdf70b57",
+    "url": "/static/js/5.adbd66fc.chunk.js"
+  },
+  {
+    "revision": "72c4747689a9b3bda90f",
+    "url": "/static/js/6.b91655cb.chunk.js"
+  },
+  {
+    "revision": "b2ae83966175ca5eae75",
+    "url": "/static/js/7.96443cf4.chunk.js"
+  },
+  {
+    "revision": "e27ea73c22911095c03d",
+    "url": "/static/js/main.e4e2abdf.chunk.js"
+  },
+  {
+    "revision": "5d4e788c363ff6e95584",
+    "url": "/static/js/runtime-main.13533ade.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jcw-app/precache-manifest.13ae09e61376489630c4bed92c967a03.js"
+  "/precache-manifest.dc4937a0bd310e932dac63c2a12d9a14.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jcw-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
